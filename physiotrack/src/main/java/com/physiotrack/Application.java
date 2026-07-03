e1be63bee6b8aed1805217de9b14e4516672c518
MartinVozmediano <martin.vozmediano@udla.edu.ec>
2026-07-03 17:58:40 -0500

    Refactor Application class by removing unused imports and style annotations

--- physiotrack/src/main/java/com/physiotrack/Application.java
+++ physiotrack/src/main/java/com/physiotrack/Application.java
@@ -1,21 +1,15 @@
 package com.physiotrack;
 
-import com.vaadin.flow.theme.aura.Aura;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.vaadin.flow.component.dependency.StyleSheet;
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.component.page.Push;
-
 @SpringBootApplication
-@StyleSheet(Aura.STYLESHEET)
-@StyleSheet("styles.css") // Your custom styles
 @Push
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
